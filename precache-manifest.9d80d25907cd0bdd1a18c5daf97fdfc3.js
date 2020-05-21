0b7d4b12146675120bf48d8f0b3cac8f8669baa4
Cinthia Pi7 <49377882+CINPIS@users.noreply.github.com>
2020-05-21 17:59:39 -0300

    Updates

---
+++ precache-manifest.9d80d25907cd0bdd1a18c5daf97fdfc3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6700220133a716e9806a55ebfa659a0",
+    "url": "/robots/index.html"
+  },
+  {
+    "revision": "94116b34afdabe876010",
+    "url": "/robots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bdb2ba2369359082470a",
+    "url": "/robots/static/css/main.15da7dd8.chunk.css"
+  },
+  {
+    "revision": "94116b34afdabe876010",
+    "url": "/robots/static/js/2.9b8d6c50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robots/static/js/2.9b8d6c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdb2ba2369359082470a",
+    "url": "/robots/static/js/main.652aae89.chunk.js"
+  },
+  {
+    "revision": "8f4c2c81f213f98c09f2",
+    "url": "/robots/static/js/runtime-main.2b48a6a5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
